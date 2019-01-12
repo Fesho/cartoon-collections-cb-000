b9e4160cd37c193c06d0b75f0ef95c01afc6d641
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-12 13:24:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,18 +9,20 @@ end
 def summon_captain_planet(planeteer_calls)
 new_array=[]
 planeteer_calls.collect do |element|
- new_array.push (element.upcase + "!") 
+ new_array.push (element.upcase + "!")
 end
 puts new_array
 end
-summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
 end
+end
+
 
 def find_the_cheese(cheese_types)
 puts cheese_types.include?("gouda" && "cheddar")
-  
+
 end
 find_the_cheese(["tomato soup", "cheddar", "oyster crackers", "gouda"])
